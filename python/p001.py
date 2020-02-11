38d7128c037ad908fe3d1292e68339e0eeebf746
Floozutter <floozutter@gmail.com>
2020-02-11 02:28:51 -0800

    Update style and performance

--- python/p001.py
+++ python/p001.py
@@ -1,3 +1,33 @@
-print(sum(
-    set(range(0, 1000, 3)) | set(range(0, 1000, 5))
-))
+"""
+Project Euler - Problem 1
+"Find the sum of all the multiples of 3 or 5 below 1000."
+"""
+
+def sum_multiples(divisor: int, terms: int) -> int:
+    """
+    Sums the first N multiples of a divisor.
+    Uses the arithmetic series formula.
+    """
+    return divisor * (terms * (terms + 1)) // 2
+
+def fizzbuzzsum(upperbound: int) -> int:
+    """
+    Sums every multiple of 3 or 5 below an upper bound.
+    This is achieved by summing the 3-multiples with the 5-multiples, then
+    subtracting the 15-multiples from that to remove duplicated terms.
+    """
+    fizzsum      = sum_multiples( 3, (upperbound-1) //  3)
+    buzzsum      = sum_multiples( 5, (upperbound-1) //  5)
+    intersectsum = sum_multiples(15, (upperbound-1) // 15)
+    return fizzsum + buzzsum - intersectsum
+
+
+def answer() -> str:
+    """
+    Returns the answer to the problem as a string.
+    """
+    return str(fizzbuzzsum(1000))
+
+
+if __name__ == "__main__":
+    print(answer())
--- python/p002.py
+++ python/p002.py
@@ -1,7 +1,33 @@
-total = 0
-last, curr = 1, 2
-while curr < 4000000:
-    if curr % 2 == 0:
+"""
+Project Euler - Problem 2
+"By considering the terms in the Fibonacci sequence whose values do not exceed
+four million, find the sum of the even-valued terms."
+"""
+
+def evenfibsum(upperbound: int) -> int:
+    """
+    Sums every even Fibonacci number below an upper bound.
+    This implementation avoids checking integer parity by leveraging the
+    mathematical fact that every third Fibonacci number is even.
+    This insight was taken from the user Begoner from the problem's discussion
+    thread on the Project Euler website.
+    """
+    total = 0
+    last, curr = 1, 2
+    while curr < upperbound:
         total += curr
-    last, curr = curr, last+curr
-print(total)
+        last, curr = curr, last+curr  # curr will be odd
+        last, curr = curr, last+curr  # curr will be odd, again
+        last, curr = curr, last+curr  # curr will be even
+    return total
+
+
+def answer() -> str:
+    """
+    Returns the answer to the problem as a string.
+    """
+    return str(evenfibsum(4_000_000))
+
+
+if __name__ == "__main__":
+    print(answer())
