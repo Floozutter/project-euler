576bc6770f639ee27102dd68926670ca89371861
Floozutter <floozutter@gmail.com>
2020-03-15 03:13:13 -0700

    Fix problem number in docstring

--- python/p008.py
+++ python/p008.py
@@ -1,5 +1,5 @@
 """
-Project Euler - Problem 7
+Project Euler - Problem 8
 "Find the thirteen adjacent digits in the 1000-digit number that have the
 greatest product. What is the value of this product?"
 """
